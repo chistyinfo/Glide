542511750bc9ff31a7fde11292aaaba6909da88a
chisty <chistyinfo@gmail.com>
2016-05-04 21:31:22 +0600

    Initial commit

---
+++ app/src/main/java/greendust/glide/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package greendust.glide.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import greendust.glide.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    String imgUrl = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
